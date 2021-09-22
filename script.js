7c393cb95416e63a62503681edded601aaa23258
NixonXC <81410474+NixonXC@users.noreply.github.com>
2021-09-22 16:51:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("fm").onclick = function ()
+{
+    p = document.createElement("p"); p.className='btn'; p.v=0;
+    p.innerHTML = "YOU JUST GOT FREE DISCORD NITRO LOL JK ";
+    document.body.appendChild(p);
+}
